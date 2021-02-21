372859d9cf62d5187f1bfdb71613c1a53ab3814d
joyce Passananti <joycep@uchicago.edu>
2021-02-20 18:09:51 -0600

    login screen

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -7,7 +7,7 @@ import { Text, View } from '../components/Themed';
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>User profile</Text>
+      <Text style={styles.title}>User profile: </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/ProfileScreen.tsx" />
     </View>
